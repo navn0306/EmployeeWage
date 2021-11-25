8e486d3a0a3a415b1051457cfe42ab75923e5c1b
Navneet <navn0306@gmail.com>
2021-11-26 00:40:18 +0530

    ADD: Added User case 1

---
+++ src/com/bridge/EmployeeWage/EmployeeWage.java
@@ -0,0 +1,14 @@
+package com.bridge.EmployeeWage;
+
+public class EmployeeWage {
+
+    public static void main(String[] args) {
+
+        int IS_FULL_TIME = 1;
+        double empCheck = Math.floor(Math.random() * 10);
+        if (empCheck == IS_FULL_TIME)
+            System.out.println("Employee is present");
+        else
+            System.out.println("Employee is absent");
+    }
+}
