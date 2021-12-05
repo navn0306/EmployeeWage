1bd9670096b4c30585cea7ac206f39baf91f1321
Navneet <navn0306@gmail.com>
2021-12-05 16:00:26 +0530

    Computing Employee Wage for Multiple Company in Procedural Way Using Class Methods

--- src/com/bridge/EmployeeWage/EmployeeWage.java
+++ src/com/bridge/EmployeeWage/EmployeeWage.java
@@ -3,17 +3,12 @@ package com.bridge.EmployeeWage;
 public class EmployeeWage {
     public static final int IS_PART_TIME = 1;
     public static final int IS_FULL_TIME = 2;
-    public static final int EMP_RATE_PER_HOUR = 20;
-    public static final int NUM_OF_WORKING_DAYS = 20;
-    public static final int MAX_HRS_IN_MONTH = 100;
 
+    public static int computeEmpWage(String company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMonth) {
 
-    public static int calcEmpWageForCompany(String company, int empRate, int numOfDays, int maxHrs) {
-        //VARIABLES
         int empHrs = 0, totalEmpHrs = 0, totalWorkingDays = 0;
 
-
-        while (totalEmpHrs <= maxHrs && totalWorkingDays < numOfDays) {
+        while (totalEmpHrs <= maxHoursPerMonth && totalWorkingDays < numOfWorkingDays) {
 
             totalWorkingDays++;
 
@@ -30,17 +25,16 @@ public class EmployeeWage {
                     empHrs = 0;
             }
             totalEmpHrs += empHrs;
+            System.out.println("Day# " +totalWorkingDays+"Emp Hr "+empHrs);
         }
-        int totalEmpWage = totalEmpHrs * empRate;
+        int totalEmpWage = totalEmpHrs * empRatePerHour;
         System.out.println("Total Emp Wage for company : " + company + " is :" + totalEmpWage);
         return totalEmpWage;
 
     }
 
     public static void main(String[] args) {
-
-        int totalWage1 = calcEmpWageForCompany("Dmart", 20, 2, 10);
-        int totalWage2 = calcEmpWageForCompany("Reliance", 10, 4, 20);
-
+        computeEmpWage("Dmart",20,3,10);
+        computeEmpWage("Reliance",10,4,20);
     }
 }
