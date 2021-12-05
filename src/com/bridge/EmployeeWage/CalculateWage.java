a2b5b2fc094014f6c200d960c85cf0cc37dbf3fe
Navneet <navn0306@gmail.com>
2021-12-05 14:02:41 +0530

    Refactor: Added method to calculate employeewage for twio diifrent companies

--- src/com/bridge/EmployeeWage/CalculateWage.java
+++
@@ -1,7 +0,0 @@
-package com.bridge.EmployeeWage;
-
-public class CalculateWage extends EmployeeWage {
-    public static void main(String[] args){
-        calculateWage();
-    }
-}
--- src/com/bridge/EmployeeWage/EmployeeWage.java
+++ src/com/bridge/EmployeeWage/EmployeeWage.java
@@ -7,11 +7,13 @@ public class EmployeeWage {
     public static final int NUM_OF_WORKING_DAYS = 20;
     public static final int MAX_HRS_IN_MONTH = 100;
 
-    public static void calculateWage() {
-        int empHrs = 0, totalEMPHRS = 0, totalWorkingDays = 0;
+
+    public static int calcEmpWageForCompany(String company, int empRate, int numOfDays, int maxHrs) {
+        //VARIABLES
+        int empHrs = 0, totalEmpHrs = 0, totalWorkingDays = 0;
 
 
-        while (totalEMPHRS <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS) {
+        while (totalEmpHrs <= maxHrs && totalWorkingDays < numOfDays) {
 
             totalWorkingDays++;
 
@@ -27,9 +29,18 @@ public class EmployeeWage {
                 default:
                     empHrs = 0;
             }
-            totalEMPHRS += empHrs;
+            totalEmpHrs += empHrs;
         }
-        int total_Emp_Wage = totalEMPHRS * EMP_RATE_PER_HOUR;
-        System.out.println("Total Emp Wage: " + total_Emp_Wage);
+        int totalEmpWage = totalEmpHrs * empRate;
+        System.out.println("Total Emp Wage for company : " + company + " is :" + totalEmpWage);
+        return totalEmpWage;
+
+    }
+
+    public static void main(String[] args) {
+
+        int totalWage1 = calcEmpWageForCompany("Dmart", 20, 2, 10);
+        int totalWage2 = calcEmpWageForCompany("Reliance", 10, 4, 20);
+
     }
 }
